7e3e732eff573e36484d64f8c61813a6fa83edbe
Priyanka-Barole <priyanakabarole888@gmail.com>
2023-12-28 12:42:07 +0530

    add file

---
+++ sum_of_natural_no.py
@@ -0,0 +1,7 @@
+#write a program to print sum of frst natural numbers.
+sum_number=int(input("enter the frst natural number:"))
+i=1
+while sum_number<=1:
+	result=result+sum_number
+	print(result)
+	i-=1
