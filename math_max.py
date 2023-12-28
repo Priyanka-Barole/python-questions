b29a7954a19935e25f76283aa3a72aeb74ece462
Priyanka-Barole <priyanakabarole888@gmail.com>
2023-12-28 13:02:51 +0530

    add file

---
+++ math_max.py
@@ -0,0 +1,12 @@
+# Que-write a python program create math maeks.
+math_marks=int(input("enter the math marks:"))
+if math_marks>90:
+	print("gread,A")
+elif math_marks>80 and math_marks<=90:
+	print("gread,B")
+elif math_marks>70 and math_marks<=60:
+	print("gread,c")
+elif math_marks>60 and math_marks<=50:
+	print("gread,D")
+else :
+	print("Fail")			
