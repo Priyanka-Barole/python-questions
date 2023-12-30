d5a163751fa5b6a219859fce6ff29fb63d61dfac
Priyanka-Barole <priyanakabarole888@gmail.com>
2023-12-30 12:50:41 +0530

    add file

---
+++ Hello_Bay.py
@@ -0,0 +1,8 @@
+#write a program to display "Hello" if a number entered by user is a multipul of five and print"Bye".
+num=int(input("Enter the number:"))
+if num%5==0:
+	print("Hello")
+else:
+	print("Bye")
+
+		
