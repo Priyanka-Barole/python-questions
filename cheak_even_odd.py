117a6dc312671a4590add8c625248f2bf7b9850c
Priyanka-Barole <priyanakabarole888@gmail.com>
2023-12-30 13:00:49 +0530

    add file

---
+++ cheak_even_odd.py
@@ -0,0 +1,7 @@
+# Que-write a program cheak whethera number a number is even or odd.
+var1=int(input("enter value:"))
+varx = var1%2
+if varx==0:
+	print("var1 is even")
+else:
+	print("var1 is odd")
