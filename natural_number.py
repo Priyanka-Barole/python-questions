1e75e0749dc96896d05c29a148a3aad472c9a2ed
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-01 12:58:15 +0530

    add file

---
+++ natural_number.py
@@ -0,0 +1,18 @@
+# 1.Que-write a python program print first 10 natural number.
+num=int(input("enter number:"))
+for i in range(1,11):
+	print(i)
+
+# 6.que-write a python program print table of a number accepted from user.
+num=int(input("enter number:"))
+for i in range(1,11):
+	print(num*i)
+			
+
+
+
+
+
+
+
+
