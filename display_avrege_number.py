f86198cedc919dab5fa9b38b8f3f32064082544f
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-01 12:55:53 +0530

    add file

---
+++ display_avrege_number.py
@@ -0,0 +1,6 @@
+#9.que-write a python program accept 10 number from the user display their avrage.
+'''num=int(input("enter number:"))
+result=0
+for i in range(10):
+	result=result+num
+	print("avrege of 10 number is" ,result)	'''
