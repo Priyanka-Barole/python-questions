c1e3da1e1d53434d2a04e4623e83fafd9d533ff0
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-01 18:29:28 +0530

    add file

---
+++ even_odd_number.py
@@ -0,0 +1,6 @@
+# que-write a python program even number in front of print(even) and odd number in front of print(odd) 
+for i in range(1,101):
+    if i % 2 == 0:
+        print(i , "E")
+    else:
+        print(i , "O")
