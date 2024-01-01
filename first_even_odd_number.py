02bf54d2008ae172743aafb7d7bfbcd7cd17c1d1
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-01 12:54:26 +0530

    add file

---
+++ first_even_odd_number.py
@@ -0,0 +1,19 @@
+# 2.Que-write a python progarm print  frist 10 even number.
+num=int(input("enter number:"))
+for i in range(2,22,2):
+	print(i)
+
+# 3.Que-write a python program print frist 10 odd number.
+num=int(input("enter number:"))
+for i in range(1,21,2):
+	print(i)
+
+# 4.write a python program print frist 10 reverse even number.
+num=int(input("enter number:"))
+for i in range(20,0,-2):
+	print(i)	
+
+# 5.que-write a python program print frist 10 reverse odd number.
+num=int(input("enter number:"))
+for i in range(21,1,-2):
+	print(i)	
