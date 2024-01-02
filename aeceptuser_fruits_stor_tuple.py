092d24297e7955b033c4792228e8288f9d65cf96
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-02 13:37:03 +0530

    add file

---
+++ aeceptuser_fruits_stor_tuple.py
@@ -0,0 +1,8 @@
+# que-write a python program to accept five fruits name for user the stor in tuple.
+T1=()
+result=0
+while result<5:
+	fruits_name=input("enter fruits_name:")
+	T1=T1+(fruits_name,)
+	result=result+1
+	print(T1)
