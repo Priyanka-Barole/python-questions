a0edd7c1d94bb5c602962467fb67548869991eab
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-02 13:39:29 +0530

    add file

---
+++ tuple_program.py
@@ -0,0 +1,32 @@
+# Que- write the output follwing code.
+T1=(6)
+print(type(T1))
+
+# Que- write the output follwing code.
+T1=(1,2,3,4,5,6)
+print(T1)
+print(T1*2)
+print(T1+T1)
+print(len(T1)*2)
+
+# Que- write the output follwing code.
+T1=(1,2,3,4,5,6,7,8)
+print(T1[0])
+print(T1[-1])
+print(T1[2+3])
+print(T1[4-1])
+print(T1[7%2])
+
+# Que- write the output follwing code.
+T1=(45,67,98)
+T1=T1+(1,2,3)
+print(T1)
+
+# Que- write the output follwing code.
+T1=(45,67,98)
+T2=(45,67,98)
+print(T1 in T2)
+print(45 in T2)
+print(45 in T2)
+print(T1+T2)
+
