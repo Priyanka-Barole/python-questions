3c9ce0e18a5ef8c25e89e8f06d4e3d64e80cf6ab
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-03 12:40:48 +0530

    add file

---
+++ simple_table_create.py
@@ -0,0 +1,12 @@
+# que-write a python program create to simple table.
+var_1=int(input("enter any value:"))
+var_2=int(input("How many iteration:"))
+for var_x in range(1,var_2):
+	print(var_1,"*",var_x,"=",var_1*var_x)
+'''
+'''
+# que-write a python program create to simple table. 
+num_x=int(input("enter any number:"))
+num_y=int(input("How many iteration:"))
+for num_z in range(1,num_y):
+	print(num_x,"*",num_z,"=",num_x*num_y)
