242f20e0fbfd0038a599ccd1713681501166efdc
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-03 17:26:32 +0530

    add file

---
+++ acepte_fivenumber_in_list.py
@@ -0,0 +1,13 @@
+'''# que-write a python proram acepte five number from user and stor in list.
+lst=[]
+for i in range(5):
+	user_number=int(input("enter the number:"))
+	lst.append(user_number)
+	print(lst)
+'''
+# que-write a python proram acepte five number from user and stor in list.
+lst=[]
+for i in range(5):
+	fruits_name=input("enter fruits name:")
+	lst.append(fruits_name)
+	print(lst)
