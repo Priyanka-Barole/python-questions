7c439733c7d47885b4db4d1ce9f8f67199f4f5fe
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-03 12:41:57 +0530

    add file

---
+++ simple_fectorial_create.py
@@ -0,0 +1,13 @@
+# que-write a python program find the fecactoral of number.
+var_x=int(input("enter a value:"))
+result=1
+for i in range(1,var_x+1):
+	result=result*i
+print(result) 
+
+# que-write a python program find the fecatoral of number.	
+var_x=int(input("enter a value:"))
+result=1
+for i in range(1,var_x-1):
+	result=result*i
+print(result)
