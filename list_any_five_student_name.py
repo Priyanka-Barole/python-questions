6b80f69229b3e3e12039b3c7604a8bbda972e0b0
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-03 17:28:16 +0530

    add file

---
+++ list_any_five_student_name.py
@@ -0,0 +1,14 @@
+# Que-write a python program to create a follwing list(take input form the user)
+'''# any five student name
+lst=[]
+student_name=input("enter student name:")
+for i in range(5):
+	lst.append(student_name)
+print(lst)'''
+
+# any five number
+lst=[]
+num=input("enter number:")
+for i in range(5):
+	lst.append(num)
+print(lst)	
