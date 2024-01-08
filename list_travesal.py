9d0c98f658f932df988a3dc770bc42813dc068db
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-08 18:18:50 +0530

    add file

---
+++ list_travesal.py
@@ -0,0 +1,9 @@
+# que-write a python program create a teavesal list using for loop.
+'''lst=[10,20,30,40,50]
+for i in lst:
+	print(i)
+'''
+# que- write a python program cretate a teavesal list using len()and range().
+lst=[10,20,30,40,50]
+for i in range(len(lst)):
+	print(lst[i])		
