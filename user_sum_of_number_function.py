e3cca3bd8fe3f0bda2d1c67f6b2bedc34cabd73b
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-08 18:00:56 +0530

    add file

---
+++ user_sum_of_number_function.py
@@ -0,0 +1,8 @@
+# Que- fonction which take 2 number  as user input and return thire sum.
+def add(n1,n2):
+	ans=n1+n2
+	return ans
+n1=int(input("enter n1:"))
+n2=int(input("enter n2:"))	
+output=add(n1,n2)
+print("sum of the number",add(n1,n2))
