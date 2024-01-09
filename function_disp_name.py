f73ec06336ccff9641cd71fe8095418a973d09ec
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-09 18:28:21 +0530

    add file

---
+++ function_disp_name.py
@@ -0,0 +1,17 @@
+# que-write a python program user difined function to add two numbers and display their sum. 
+def sum2num():
+	num_1=int(input("enter first number:"))
+	num_2=int(input("enter secend number:"))
+	n=num_1+num_2
+	print("the sum is",n)
+# function call
+sum2num()
+
+# write a python program user difind function "rec_ar"to calculate the area of rectangle.
+def rec_ar():
+	length=int(input("enter length of rectengle:"))
+	breath=int(input("enter breath of rectengle:"))
+	Area_of_rectengle=length*breath
+	print(" The Area of rectengle is",Area_of_rectengle)
+# function call
+rec_ar()			
