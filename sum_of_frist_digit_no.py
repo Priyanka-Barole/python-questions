160c0aab70c37f19f7267e003996e8049f007690
Priyanka-Barole <priyanakabarole888@gmail.com>
2024-01-09 18:44:49 +0530

    add file

---
+++ sum_of_frist_digit_no.py
@@ -0,0 +1,7 @@
+# 10.que-write a python program find sum of the first digite number.
+num=input("enter number:")
+result=0
+for i in range (len(num)):
+	first_digit=int(num[i])
+	result=result+frist_digit
+print(result)
